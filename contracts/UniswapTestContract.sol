1ad098c5da3119c93676379e5ca06bdb3318d0e0
Alessandro <defranceschi_a@yahoo.com>
2025-01-23 17:14:47 +0100

    fixes

--- contracts/UniswapTestContract.sol
+++ contracts/UniswapTestContract.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract UNISWAP is Ownable {
     constructor() {}
@@ -26,9 +27,14 @@ contract UNISWAP is Ownable {
         address to,
         uint deadline
     ) public returns (uint, uint, uint) {
-        uint x = amountA - 1;
-        uint y = amountB - 1;
-        return (x, y, 1);
+        IERC20 erc20TokenA = IERC20(tokenA);
+        IERC20 erc20TokenB = IERC20(tokenB);
+        uint usedAmountA = amountA - 100;
+        uint usedAmountB = amountB - 100;
+        erc20TokenA.transferFrom(msg.sender, address(this), usedAmountA);
+        erc20TokenB.transferFrom(msg.sender, address(this), usedAmountB);
+
+        return (usedAmountA, usedAmountB, 1);
     }
 
     function swapExactTokensForTokens(
@@ -41,6 +47,8 @@ contract UNISWAP is Ownable {
         uint[] memory result = new uint[](2);
         result[0] = 0;
         result[1] = (amount / 10 ** 18) * 10 ** 6;
+        IERC20(path[0]).transferFrom(msg.sender, address(this), amount);
+        IERC20(path[1]).transfer(to, result[1]);
         return result;
     }
 }
