84f98c0582ea1716829ed1e65c4596fed97d910c
Alessandro <defranceschi_a@yahoo.com>
2025-01-24 15:04:35 +0100

    ERC20Mock set 6 decimals

--- contracts/ERC20Mock.sol
+++ contracts/ERC20Mock.sol
@@ -4,34 +4,38 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Mock is ERC20("Mock", "MK") {
-	bool public transferFromCalled = false;
+    bool public transferFromCalled = false;
 
-	bool public transferCalled = false;
-	address public transferRecipient = address(0);
-	uint256 public transferAmount = 0;
+    bool public transferCalled = false;
+    address public transferRecipient = address(0);
+    uint256 public transferAmount = 0;
 
-	function mint(address user, uint256 amount) public {
-		_mint(user, amount);
-	}
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
 
-	function transferFrom(
-		address sender,
-		address recipient,
-		uint256 amount
-	) public virtual override returns (bool) {
-		transferFromCalled = true;
+    function mint(address user, uint256 amount) public {
+        _mint(user, amount);
+    }
 
-		return super.transferFrom(sender, recipient, amount);
-	}
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        transferFromCalled = true;
 
-	function transfer(
-		address recipient,
-		uint256 amount
-	) public virtual override returns (bool) {
-		transferCalled = true;
-		transferRecipient = recipient;
-		transferAmount = amount;
+        return super.transferFrom(sender, recipient, amount);
+    }
 
-		return super.transfer(recipient, amount);
-	}
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        transferCalled = true;
+        transferRecipient = recipient;
+        transferAmount = amount;
+
+        return super.transfer(recipient, amount);
+    }
 }
