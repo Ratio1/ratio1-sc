12cc5630bc8016a290984155022b646b50f4d4d3
Alessandro <alessandro.defranceschi@dbcodingsrl.com>
2026-04-28 16:34:47 +0300

    chore: change sepolia rpc

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -35,7 +35,7 @@ const config: HardhatUserConfig = {
     },
     baseSepolia: {
       chainId: 84532,
-      url: "https://base-sepolia-rpc.publicnode.com",
+      url: "https://sepolia.base.org",
       accounts,
     },
     base: {
